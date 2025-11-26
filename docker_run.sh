08dfa99c5d70800984770a8ccebc8acfb1684893
Buraks <sevsayburak@gmail.com>
2025-11-26 11:15:37 +0100

    Fix the repo for ros noetic

---
+++ docker_run.sh
@@ -0,0 +1,19 @@
+docker run --rm -it \
+  --name ros-noetic-rviz \
+  --gpus all \
+  --network host \
+  --ipc=host \
+  -e DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -e __GLX_VENDOR_LIBRARY_NAME=nvidia \
+  -e __NV_PRIME_RENDER_OFFLOAD=1 \
+  -e NVIDIA_VISIBLE_DEVICES=all \
+  -e NVIDIA_DRIVER_CAPABILITIES=compute,utility,graphics,display,video \
+  -e XDG_RUNTIME_DIR=/run/user/$(id -u) \
+  -e ROS_DOMAIN_ID=77 \
+  -v /run/user/$(id -u):/run/user/$(id -u) \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $HOME/.Xauthority:/root/.Xauthority:ro \
+  -v /home/buraks/workspace:/data:rw \
+  osrf/ros:noetic-desktop-full \
+  bash
